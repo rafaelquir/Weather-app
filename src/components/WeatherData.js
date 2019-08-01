d7acf72676cd9ebb444592fae8718aadcfa56e30
Rafael Quiroz <rafael_quirozve@hotmail.com>
2019-08-01 20:42:51 +0200

    Adelantando el curso

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import WeatherExtraInfo from './WeatherExtraInfo';
+import WeatherTemparature from './WeaherTemperature';
+
 
 const WeatherData = () => (
-     <div>Weather Dara</div>
+     <div>
+         <WeatherTemparature/>
+         <WeatherExtraInfo humidity ={80} wind = {"10 m/s"} />
+     </div>
 );
+
 
 export default WeatherData;
