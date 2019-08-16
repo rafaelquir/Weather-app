c5cace8858e12969fbb99f4753e6451837448b86
Rafael Quiroz <rafael_quirozve@hotmail.com>
2019-08-16 18:55:05 +0200

    Trabajando con Redux

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
-import { Grid, Row, Col } from 'react-flexbox-grid';
 import LocationList from './components/LocationList';
 import ForecastExtended from './components/ForecastExtended';
+import { setCity } from './actions';
+import { store } from './store';
+
 import './App.css';
-
-
 
 const cities = [
   "Caracas,ve",
@@ -18,21 +19,25 @@ const cities = [
   "Mexico,mx",
   "Bogota,co",
 ];
+
 class App extends Component {
 
   constructor() {
     super();
     this.state = {city: null};
   }
+
   handleSelectedLocation = city =>{
     this.setState({ city });
-      console.log(`handleSelectedLocation ${city}`);
+    console.log(`handleSelectedLocation ${city}`);
+
+    store.dispatch(setCity(city));
   }
 
   render (){
     const { city } = this.state;
     return (
-     
+      
         <Grid>
           <Row>
             <AppBar position="sticky">
@@ -62,7 +67,7 @@ class App extends Component {
           </Row>
          
         </Grid>
-      
+   
     );
   }
 }
